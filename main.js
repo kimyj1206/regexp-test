343f7b9e865dffe5b5ff2b8de11139618e5a8e5a
kimyj1206 <kimyj1592@naver.com>
2023-06-24 21:37:14 +0900

    create project

---
+++ main.js
@@ -0,0 +1,22 @@
+const str = `
+010-1234-5678
+thekyj@naver.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+http://localhost:1234
+동해물과_백두산이 마르고 닳도록
+`
+
+// console.log(str.match(/\b\w{2,3}\b/g));
+// console.log(str.match(/\w/g));
+// console.log(str.match(/\bf\w{1,}\b/g));
+// console.log(str.match(/\d{1,}/g));
+
+// const h = `  the hello world   !
+
+// `
+// console.log(h.replace(/\s/g, ''));
+
+// console.log(str.match(/.{1,}(?=@)/g));
+// console.log(str.match(/(?<=@).{1,}/g));
